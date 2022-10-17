6bedd5d353cc22cf771f3cab7fe1848b260a4b6d
Allan-Gabriel <40467491+Allan-Gabriel@users.noreply.github.com>
2022-10-17 16:38:20 -0700

    Changes to original code. Also added stroring and loading mechanism

--- COSC 310/src/proj/Product.java
+++ COSC 310/src/proj/Product.java
@@ -1,87 +1,61 @@
 package proj;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Scanner;
 //import currentDateTime checker to check if holidays are coming up
-public class Product {
-	 String name;
-	 int expDate;
+public class Product implements Serializable{
+	 private static final long serialVersionUID = 1L;
+	String name;
+	 String expDate; //Changed date to string value,later improve with string patterns to check correct dates
 	 int quant;
 	 double value;
 
 	/*static ArrayList<String> inv = new ArrayList<>();
 	static ArrayList<String> inv1 = new ArrayList<>();*/
-	Product(String name, int expDate, int quant, int value) {
+	Product(String name, String expDate, int quant, int value) {
 		this.name = name;
 		this.expDate = expDate;
 		this.quant = quant;
 		this.value = value;
 	}
+
+	//generated the getters and setter again for the class attributes
 	public String getName() {
 		return name;
 	}
-	public String getDate() {
-		String date = String.valueOf(expDate);
-		return date;
+
+
+	public void setName(String name) {
+		this.name = name;
 	}
+
+
+	public String getExpDate() {
+		return expDate;
+	}
+
+
+	public void setExpDate(String expDate) {
+		this.expDate = expDate;
+	}
+
+	public int getQuant() {
+		return quant;
+	}
+	public void setQuant(int quant) {
+		this.quant = quant;
+	}
+	public double getValue() {
+		return value;
+	}
+	public void setValue(double value) {
+		this.value = value;
+	}
+	
+
+	//Modified toString method
 	public String toString() {
-		String e = getName() + " " + getDate();
-		return e;
-	}
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		ArrayList<Object> arr = new ArrayList<>();
-		int fin = 0;
-		while(fin ==0){
-			System.out.println("Would you like to add an item? (1)");
-			System.out.println("Would you like to remove an item? (2)");
-			System.out.println("Would you like to see the inventory? (3)");
-			int t = sc.nextInt();
-		switch(t){
-			case 1: 
-				System.out.println("enter product name: ");
-				String q = sc.next();
-				System.out.println("enter product expiration Date: ");
-				int w = sc.nextInt();
-				System.out.println("enter product quantity: ");
-				int e = sc.nextInt();
-				System.out.println("enter product Value (After tax): ");
-				int r = sc.nextInt();
-				Product p1 = new Product(q, w, e, r);
-				arr.add(p1);
-				System.out.println("Succesfully added item: "+p1);
-				break;
-			case 2: 
-				System.out.println("What item would you like to remove? ");
-				String rem = sc.next();
-				if(arr.contains(rem)) {
-					int ind = arr.indexOf(rem);
-					arr.remove(ind);
-				}
-				else {
-					System.out.println("The item does not exist");
-				}
-				break;
-			case 3:
-				System.out.println(arr);
-				break;		
-		}
-		System.out.println("Would you like to preferom another action? (Y/N) ");
-		String end = sc.next();
-		if(end == "Y")
-			fin = 0;
-		else;
-			fin = 1;
-		}
-		System.out.println("finished");
-		/*String q = Integer.toString(expDate);
-		String w = Integer.toString(quant);
-		String e = Integer.toString(value);
-		inv.add(q);
-		inv.add(w);
-		inv.add(e);
-		inv.add(name);
-		inv1.addAll(inv);*/
-		sc.close();
+		return getName() + ", " + getExpDate()+", "+getQuant()+", "+getValue();
 	}
 }
