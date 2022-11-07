3b93a2ec861eec65251f38941e578d7c82d56a21
Allan-Gabriel <40467491+Allan-Gabriel@users.noreply.github.com>
2022-11-07 11:33:58 -0800

    Added a database

---
+++ COSC 310/src/proj/Supplier.java
@@ -0,0 +1,33 @@
+package proj;
+/**
+ * A class to represent the different suppliers of the products
+ */
+public class Supplier {
+	private String supplierName;
+	private String supplierEmail;
+	private String product;
+	
+	
+	public String getSupplierName() {
+		return supplierName;
+	}
+	public void setSupplierName(String supplierName) {
+		this.supplierName = supplierName;
+	}
+	public String getSupplierEmail() {
+		return supplierEmail;
+	}
+	public void setSupplierEmail(String supplierEmail) {
+		this.supplierEmail = supplierEmail;
+	}
+	public String getProduct() {
+		return product;
+	}
+	public void setProduct(String product) {
+		this.product = product;
+	}
+	
+	
+	
+
+}
--- COSC 310/src/proj/test.java
+++ COSC 310/src/proj/test.java
@@ -1,17 +1,17 @@
 package proj;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
-		ProductList arr = new ProductList("prodList.dat");
+		db app = new db();//creates an object of the db class so we can utilise its functions
+		app.connect();//Creates a connection to the database
 		boolean end=false;
 		while(!end){
-			System.out.println("Would you like to add an item? (1)");
-			System.out.println("Would you like to remove an item? (2)");
+			System.out.println("Would you like to add a Product? (1)");
+			System.out.println("Would you like to remove a Product? (2)");
 			System.out.println("Would you like to see the inventory? (3)");
 			System.out.println("Would you like the check if any of your inventory is low? (4)");
 			System.out.println("Would you like to remove specific amount from an items quantity (5)");
@@ -20,31 +20,31 @@ public class test {
 			case 1: 
 				System.out.println("enter product name: ");
 				String q = sc.next();
-				System.out.println("enter product expiration Date: ");
+				System.out.println("enter product expiration Date(YYYY-MM-DD): ");
 				String w = sc.next();
 				System.out.println("enter product quantity: ");
 				int e = sc.nextInt();
 				System.out.println("enter product Value (After tax): ");
 				double r = sc.nextDouble();
 				Product p1 = new Product(q, w, e, r);
-				arr.add(p1);
+				app.addProduct(p1);
 				System.out.println("Succesfully added item: "+p1);
 				break;
 			case 2: //moved the logic for removing to remove function in ProductList
 				System.out.println("What item would you like to remove? ");
 				String rem = sc.next();
-				arr.remove(rem);
+				app.removeProduct(rem);
 				break;
 			case 3:
-				System.out.println(arr);
+				System.out.println(app.listProducts());
 				break;		
 			case 4:
-				System.out.println(arr.lowInventory());
+				System.out.println(app.listLowInventryProducts());
 				break;		
 			case 5:
 				System.out.println("Enter the name and quantity");
 				String d = sc.next();
-				arr.modifyQuant(d,sc.nextInt());
+				app.modifyQuantity(d,sc.nextInt());
 				break;
 		}
 		System.out.println("Would you like to perform another action? (Y/N) ");
@@ -55,7 +55,7 @@ public class test {
 			end=true;
 		}
 		sc.close();
-		arr.exit("prodList.dat");
+		app.close();
 		
 	}
 
